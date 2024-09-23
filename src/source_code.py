00252d88f209eedd4ebad1dbe5d8f60167cab5aa
Savinya03 <savinyap@gmail.com>
2024-09-23 17:35:26 +1000

    source_code.py

--- src/source_code.py
+++ src/source_code.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy import stats
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
